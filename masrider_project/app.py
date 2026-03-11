fac96b2d5192cdec2f2e99b8078a81a951b3900f
cvndifzxnrir-eng <cvndifzxnrir@gmail.com>
2026-03-11 23:53:23 +0700

    first commit

--- masrider_project/app.py
+++ masrider_project/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import psycopg2
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+
+DATABASE_URL = "postgresql://masrider_db_user:r3bPJ29oh1pNqiXi3WwBgeqLIxlpbhwC@dpg-d6oiq8ngi27c73ef992g-a.oregon-postgres.render.com/masrider_db"
 
 @app.route("/")
 def home():
-    return "Masrider Project Website Running!"
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("SELECT NOW();")
+        time = cur.fetchone()
+        cur.close()
+        conn.close()
+        return f"เชื่อมต่อ Database สำเร็จ! เวลาใน DB คือ {time}"
+    except Exception as e:
+        return f"เกิดข้อผิดพลาด: {e}"
 
-if __name__ == "__main__":
-    app.run()
+
+if _name_ == "_main_":
+    app.run(debug=True)
+
